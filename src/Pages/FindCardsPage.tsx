e2f0089efa47258433c7abb3a24f9fc179c18694
loganyeh <yehlogan@gmail.com>
2026-04-01 23:27:31 -0400

    feat: Filter Search rough draft

--- src/Pages/FindCardsPage.tsx
+++ src/Pages/FindCardsPage.tsx
@@ -11,7 +11,10 @@ function FindCardsPage(){
 
                     {/* Logo */}
                     <div className="border h-full flex justify-start items-center">
-                        <div className="border flex justify-center items-center">One Piece Card Game</div>
+                        <div className="border flex justify-center items-center font-bold text-2xl">
+                            One Piece <br />
+                            Card Game
+                        </div>
                     </div>
 
                     {/* Menu */}
@@ -25,11 +28,11 @@ function FindCardsPage(){
                 <div className="min-h-screen max-w-6xl mx-auto px-2">
 
                     {/* Card List Title */}
-                    <div className="border h-36 flex justify-center items-center text-4xl">Card List</div>
+                    <div className="h-36 flex justify-center items-center text-4xl font-bold tracking-tighter">CARD LIST</div>
 
                     {/* Filter Search */}
                     <div className="max-w-6xl px-4 bg-white shadow-md">
-                        <div className="h-full grid grid-rows-5 gap-0">
+                        <div className="h-full grid grid-rows-[1fr_1fr_1fr_auto_1fr] gap-2">
                             {/* Search */}
                             <div className="h-16 flex justify-center items-center">
                                 <div className="border-b w-full pb-1 flex justify-between items-center">
@@ -39,24 +42,39 @@ function FindCardsPage(){
                             </div>
 
                             {/* Pack Set Name */}
-                            <div className="h-16 border border-black flex justify-between">
-                                <div className="border">
+                            <div className="h-16 border-b border-black flex justify-between">
+                                <div className="font-medium">
                                     Booster Pack
                                     - Adventure on Kami's Island
                                     [OP15-EB04]<br />
                                 </div>
-                                <div className="border w-8 flex justify-center items-center shrink-0"><i className='bx bxs-down-arrow'></i></div>
+                                <div className="w-8 flex justify-center items-center shrink-0"><i className='bx bxs-down-arrow'></i></div>
                             </div>
 
 
                             {/* Add More Button */}
-                            <div className="border border-red-600 flex items-center">
-                                <div className="h-2/3 w-full bg-blue-300 rounded-3xl"></div>
+                            <div className="flex items-center">
+                                <div className="relative h-2/3 w-full bg-black flex justify-center items-center rounded-3xl">
+                                    <div className="font-semibold text-white">Add more</div>
+                                    <div className="absolute right-4 h-full flex justify-center items-center"><i className='bx bx-plus text-xl text-white' ></i></div>
+                                </div>
+                            </div>
+
+                            {/* Checkbox */}
+                            <div className="h-8 flex items-center">
+                                <div className="border-2 h-5 w-5"></div>
+                                <div className="text-sm font-medium ml-1">Hide reprint cards</div>
                             </div>
 
 
-                            <div className="border-b border-red-600"></div>
-                            <div className="border-b border-red-600"></div>
+                            {/* Search Button */}
+                            <div className="flex items-center">
+                                <div className="relative h-2/3 w-full bg-yellow-700 flex justify-center items-center rounded-3xl">
+                                    <div className="font-semibold text-white">Add more</div>
+                                    <div className="absolute right-4 h-full flex justify-center items-center"><i className='bx bx-search text-xl text-white' ></i></div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
 
