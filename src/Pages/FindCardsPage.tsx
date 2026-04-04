fb3f52e5fbf9355dade035c509269bf4c3a2456f
loganyeh <yehlogan@gmail.com>
2026-04-03 23:16:25 -0400

    feat: footer breakpoints

--- src/Pages/FindCardsPage.tsx
+++ src/Pages/FindCardsPage.tsx
@@ -324,28 +324,30 @@ function FindCardsPage(){
                 <div className="h-96 mt-16 flex justify-center items-center bg-amber-100">Luffy Splash Art</div>
 
                 {/* Footer */}
-                <div className="m-w-6xl p-4 md:p-10 lg:p-4 flex flex-col gap-4 md:gap-8 lg:grid lg:grid-cols-[repeat(20,1fr)] lg:grid-rows-[repeat(20,1fr)] bg-blue-950 text-white">
+                <div className="m-w-6xl p-4 md:p-10 lg:p-4 flex flex-col gap-4 md:gap-8 lg:gap-0 lg:grid lg:grid-cols-12 bg-blue-950 text-white">
 
                     {/* Social Media */}
-                    <div className="border flex gap-4 md:gap-16 lg:gap-4 lg:row-start-1 lg:col-start-17 lg:col-end-20 text-5xl md:text-8xl lg:text-3xl">
+                    <div className="flex gap-4 md:gap-16 lg:gap-4 lg:col-start-11 lg:col-span-2 text-5xl md:text-8xl lg:text-3xl">
                         <i className='bx bxl-facebook-circle' />
                         <i className='bx bxl-twitter' />
                         <i className='bx bxl-youtube' />
                     </div>
 
                     {/* Language */}
-                    {/* <div className="border w-fit px-4 md:px-8 lg:px-3 py-1 md:py-3.5 lg:py-1 bg-gray-900 rounded-3xl md:rounded-full flex gap-1.5 md:gap-5 lg:gap-1.5 items-center">
+                    <div className="w-fit px-4 py-1 flex items-center gap-1.5 bg-gray-900 rounded-3xl
+                    md:px-8 md:py-3.5 md:gap-5 md:rounded-full
+                    lg:h-8 lg:mt-8 lg:px-3 lg:py-1 lg:gap-1.5 lg:col-start-10 lg:col-span-4 lg:row-start-3 lg:items-start">
                         <i className='bx bx-world text-3xl md:text-5xl lg:text-xl' ></i>
                         <div className="font-light md:text-3xl lg:text-sm">English</div>
                         <div className="text-xs md:text-2xl lg:text-[10px] tracking-wider">- NA/EU/OC/LATAM/ME</div>
                         <i className='bx bxs-down-arrow text-lg md:text-2xl lg:text-[10px]' ></i>
-                    </div> */}
+                    </div>
 
                     {/* Extra Info */}
-                    {/* <div className="border flex gap-1 md:gap-2 flex-col lg:grid lg:grid-cols-3"> 
+                    <div className="flex gap-1 md:gap-2 lg:gap-0 flex-col lg:grid lg:grid-cols-4 lg:grid-rows-2 lg:col-start-6 lg:col-span-8"> 
                         {footerInfo.map((data, index) => {
                             // return <div key={index} className={`${index === 7 ? "" : "border-b-2 border-blue-600"} h-12 mt-1 flex justify-between items-center`}>
-                            return <div key={index} className={`${index === 7 ? "" : "border-b-2 md:border-b-4 lg:border border-blue-600"} py-3 md:py-6 flex justify-between lg:justify-start lg:w-fit items-center`}>
+                            return <div key={index} className={`${index === 7 ? "" : "border-b-2 md:border-b-4 lg:border-0 border-blue-600"} py-3 md:py-6 flex justify-between lg:justify-start items-center`}>
                                         <div className="flex gap-3 md:gap-8 lg:gap-3">
                                             <div className={`${index === 0 || index === 2 ? `border` : ``} h-5 w-5 md:p-5 lg:p-0 flex justify-center items-center rounded-full`}><i className={`bx ${data.icon} text-xl md:text-5xl lg:text-xl`} ></i></div>
                                             <div className="text-sm md:text-3xl lg:text-sm">{data.text}</div>
@@ -355,22 +357,24 @@ function FindCardsPage(){
                                         }
                                     </div>
                         })}
-                    </div> */}
+                    </div>
                     
                     {/* Fint Print */}
-                    {/* <div className="border flex gap-6 md:gap-10 lg:gap-6 text-[10px] md:text-xl lg:text-[10px] lg:font-light">
+                    <div className="flex gap-6 md:gap-10 lg:gap-6 lg:col-start-10 lg:col-span-3 lg:row-start-3 text-[10px] md:text-xl lg:text-[10px] lg:font-light">
                         <div>FOR INQUIRES</div>
                         <div>COOKIE POLICY</div>
-                    </div> */}
+                    </div>
 
                     {/* Logo */}
-                    <div className="border w-fit mt-4 md:mt-8 flex flex-col lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-8 items-center">
-                        <div className="text-4xl md:text-7xl font-medium">ONE PIECE</div>
-                        <div className="text-lg md:text-4xl font-extralight md:font-thin md:tracking-widest">Card Game</div>
+                    <div className="mt-4 md:mt-8 lg:mt-0 flex flex-col items-center lg:row-start-2 lg:col-start-1 lg:col-span-5">
+                        <div className="flex flex-col justify-start items-center">
+                            <div className="text-4xl md:text-7xl lg:text-6xl font-medium">ONE PIECE</div>
+                            <div className="text-lg md:text-4xl lg:text-3xl font-extralight md:font-thin md:tracking-widest">Card Game</div>
+                        </div>
                     </div>
 
                     {/* Copyright */}
-                    {/* <div className="border mt-2 md:mt-4 flex flex-col gap-5 md:gap-10 md:font-light md:tracking-wide text-[10px] md:text-xl lg:text-[10px] leading-loose">
+                    <div className="mt-2 md:mt-4 lg:mt-0 flex flex-col lg:col-start-1 lg:col-span-4 gap-5 md:gap-10 lg:gap-4 md:font-light md:tracking-wide lg:tracking-normal text-[10px] md:text-xl lg:text-[8px] leading-loose">
                         <div>
                             ©Eiichiro Oda/Shueisha<br /> 
                             ©Eiichiro Oda/Shueisha, Toei Animation
@@ -386,15 +390,15 @@ function FindCardsPage(){
                             *Google Play and the Google Play logo are trademarks or<br /> 
                             registered trademarks of Google LLC.
                         </div>
-                    </div> */}
+                    </div>
 
                     {/* Trademarks */}
-                    {/* <div className="border mt-3 md:mt-6 flex gap-3 text-4xl md:text-7xl lg:text-4xl">
+                    <div className="mt-3 md:mt-6 lg:mt-20 flex gap-3 lg:col-start-10 lg:col-span-3 lg:row-start-3 text-4xl md:text-7xl lg:text-4xl">
                         <i className='bx bxl-imdb'></i>
                         <i className='bx bxl-microsoft'></i>
                         <i className='bx bxl-adobe'></i>
                         <i className='bx bxl-slack'></i>
-                    </div> */}
+                    </div>
 
                 </div>
             </div>
